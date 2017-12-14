e287dc6db31bb4563b59582fb7ad41ec0e9cf6b7
kshithijiyer <kshithijiyer@yahoo.co.in>
2017-12-14 12:39:50 +0530

    Still working

--- linkExtract.py
+++ linkExtract.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 #linkExtract
 
 #Purpose:
@@ -11,40 +12,51 @@
 #^((http[s]?|ftp):\/)?\/?([^:\/\s]+)((\/\w+)*\/)([\w\-\.]+[^#?\s]+)(.*)?(#[\w\-]+)?$
 
 #Settings:
-#The delimeter used in the file.
-delimeter=" "
+delimeter=" "#The delimeter used in the file.
 
 #imports
 import sys, getopt
 
 def main(argv):
-   inputfile = ''
-   outputfile = ''
-   try:
-      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
-   except getopt.GetoptError:
-      print('test.py -i <inputfile> -o <outputfile>')
-      sys.exit(2)
-   for opt, arg in opts:
-      if opt == '-h':
-         print('linkExtract.py -i <inputfile> -o <outputfile>')
-         sys.exit()
-      elif opt in ("-i", "--ifile"):
-         inputfile = arg
-      elif opt in ("-o", "--ofile"):
-         outputfile = arg
-   print('Input file is ', inputfile)
-   print('Output file is ', outputfile)
-   if outputfile=="" or outputfile==" ":
-      datafile=open(inputfile,"r")
-      linkfile=open("links.txt","a")
-   elif inputfile==" " or inputfile=="":
-      print("Error: No input file provided!")
-      return
-   else:
-      datafile=open(inputfile,"r")
-      linkfile=open(outputfile,"a")
-      
+    inputfile = ''
+    outputfile = ''
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+    except getopt.GetoptError:
+        print('python3 linkExtract.py -i <inputfile> -o <outputfile>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('python3 linkExtract.py -i <inputfile> -o <outputfile>')
+            sys.exit(2)
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+        print('Input file is ', inputfile)
+        print('Output file is ', outputfile)
+        if outputfile=="" or outputfile==" ":
+            datafile=open(inputfile,"r")
+            linkfile=open("links.txt","a")
+        elif inputfile==" " or inputfile=="":
+            print("Error: No input file provided!")
+            sys.exit(2)
+        else:
+            datafile=open(inputfile,"r")
+            linkfile=open(outputfile,"a")
+            
+        #regex and writing logic goes here
+        while True:
+            line=datafile.readline()
+            if line:
+                words=line.split(delimeter)
+                for word in words:
+                    print(word)
+            else:
+                break
+        #close files goes here
+        datafile.close()
+        linkfile.close()
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
+    main(sys.argv[1:])
